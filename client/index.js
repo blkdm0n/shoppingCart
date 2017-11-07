015a92a8b4026f3cc6375d7db2e6b953d374d171
Bernard Kitchens <bernardkitchens@Bernards-MacBook-Pro.local>
2017-11-07 13:50:47 -0800

    added webpack, babel config file

---
+++ client/index.js
@@ -0,0 +1,6 @@
+
+
+ReactDOM.render(
+  <h2>Hello from REACT</h2>,
+  document.getElementById('root')
+);
--- client/scripts.js
+++
@@ -1,3 +0,0 @@
-
-
-ReactDOM.render(React.createElement('h2', null, 'Hello from REACT!!!'), document.getElementById('root'));
